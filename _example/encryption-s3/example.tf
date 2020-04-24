02f783a48dc7eb561befc19663156112080e45e0
nikitadugar <nikita@clouddrove.com>
2020-04-24 21:12:18 +0530

    enable encryption with EBS

--- _example/encryption-s3/example.tf
+++ _example/encryption-s3/example.tf
@@ -2,6 +2,34 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+module "kms_key" {
+  source                  = "git::https://github.com/clouddrove/terraform-aws-kms.git?ref=tags/0.12.4"
+  name                    = "kms"
+  application             = "clouddrove"
+  environment             = "test"
+  label_order             = ["environment", "application", "name"]
+  enabled                 = true
+  description             = "KMS key for cloudtrail"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  alias                   = "alias/cloudtrail"
+  policy                  = data.aws_iam_policy_document.default.json
+}
+
+data "aws_iam_policy_document" "default" {
+  version = "2012-10-17"
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+}
+
 module "s3_bucket" {
   source = "./../../"
 
@@ -14,5 +42,6 @@ module "s3_bucket" {
   versioning                = true
   acl                       = "private"
   bucket_encryption_enabled = true
-  sse_algorithm             = "AES256"
+  sse_algorithm             = "aws:kms"
+  kms_master_key_id         = module.kms_key.key_arn
 }
