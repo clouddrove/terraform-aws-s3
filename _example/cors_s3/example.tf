80231db1bed61d99371e49a326fb95091a862d70
HarmanJyot Kaur <harman@clouddrove.com>
2021-04-23 20:23:30 +0530

    added cors example

---
+++ _example/cors_s3/example.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "s3_bucket" {
+  source = "./../../"
+
+  name        = "clouddrove-secure-bucket"
+  environment = "test"
+  attributes  = ["public"]
+  label_order = ["name", "environment", "attributes"]
+
+  versioning     = true
+  acl            = "public"
+  cors_rule = [{
+    "allowed_headers" : ["*"] 
+    allowed_methods = ["PUT", "POST"],
+    allowed_origins = ["https://s3-website-test.hashicorp.com"],
+    expose_headers  = ["ETag"],
+    max_age_seconds = 3000 }]
+}
