2d51044c55e5a1e537cedcf720f2d87cb1132f29
yadavprakash <yadavprakash.it@gmail.com>
2021-07-08 22:40:36 +0530

    fix terratest

--- _example/logging-s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = module.s3_bucket.*.id
-  description = "The ID of the s3 bucket."
-}
-
-output "tags" {
-  value       = module.s3_bucket.tags
-  description = "A mapping of tags to assign to the S3."
-}
