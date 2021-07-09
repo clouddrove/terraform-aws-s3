be048a77b2015207e96320859a5d8eda6879f950
yadavprakash <yadavprakash.it@gmail.com>
2021-07-09 18:13:22 +0530

    fix terratest

---
+++ _example/logging-encryption-s3/example.tf
@@ -0,0 +1,31 @@
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "logging_bucket" {
+  source = "./../../"
+
+  name        = "test-logging"
+  environment = "test"
+  attributes  = ["public"]
+  label_order = ["name", "environment"]
+  acl         = "log-delivery-write"
+}
+
+
+module "s3_bucket" {
+  source = "./../../"
+
+  name        = "clouddrove-logging-encryption-bucket"
+  environment = "test"
+  attributes  = ["public"]
+  label_order = ["name", "environment"]
+
+  versioning    = true
+  acl           = "private"
+  sse_algorithm = "AES256"
+  logging       = { target_bucket : module.logging_bucket.id, target_prefix = "logs" }
+
+  depends_on = [module.logging_bucket]
+}
---
+++ _example/logging-s3/example.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "logging_bucket" {
+  source = "./../../"
+
+  name        = "logging-s3"
+  environment = "test"
+  attributes  = ["public"]
+  label_order = ["name", "environment"]
+  acl         = "log-delivery-write"
+}
+
+module "s3_bucket" {
+  source = "./../../"
+
+  name        = "clouddrove-logging-bucket"
+  environment = "test"
+  attributes  = ["public"]
+  label_order = ["name", "environment"]
+
+  versioning = true
+  acl        = "private"
+  logging    = { target_bucket : module.logging_bucket.id, target_prefix = "logs" }
+
+  depends_on = [module.logging_bucket]
+
+}
