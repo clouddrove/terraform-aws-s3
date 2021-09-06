a36fa3464588c6bf3ff8e93f597c616ea54db295
Anmol Nagpal <anmol@clouddrove.com>
2021-09-06 19:55:36 +0530

    fix changes

--- main.tf
+++ main.tf
@@ -162,40 +162,15 @@ resource "aws_s3_bucket" "s3_logging" {
     mfa_delete = var.mfa_delete
   }
 
-  lifecycle_rule {
-    id      = "transition-to-infrequent-access-storage"
-    enabled = var.lifecycle_infrequent_storage_transition_enabled
-
-    prefix = var.lifecycle_infrequent_storage_object_prefix
-
-    transition {
-      days          = var.lifecycle_days_to_infrequent_storage_transition
-      storage_class = "STANDARD_IA"
+  server_side_encryption_configuration {
+    rule {
+      bucket_key_enabled = 1
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = var.sse_algorithm
+      }
     }
   }
 
-  lifecycle_rule {
-    id      = "transition-to-glacier"
-    enabled = var.lifecycle_glacier_transition_enabled
-
-    prefix = var.lifecycle_glacier_object_prefix
-
-    transition {
-      days          = var.lifecycle_days_to_glacier_transition
-      storage_class = "GLACIER"
-    }
-  }
-
-  lifecycle_rule {
-    id      = "expire-objects"
-    enabled = var.lifecycle_expiration_enabled
-
-    prefix = var.lifecycle_expiration_object_prefix
-
-    expiration {
-      days = var.lifecycle_days_to_expiration
-    }
-  }
   logging {
     target_bucket = var.target_bucket
     target_prefix = var.target_prefix
