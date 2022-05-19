2e5c9c53004b59ac6b3c005462dc035823a26ef3
theprashantyadav <theprashantyadav7@gmail.com>
2022-05-19 13:10:58 +0530

    update version

--- _example/encryption-s3/example.tf
+++ _example/encryption-s3/example.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "kms_key" {
   source      = "clouddrove/kms/aws"
-  version     = "0.15.0"
+  version     = "1.0.1"
   name        = "kms"
   environment = "test"
   label_order = ["name", "environment"]
