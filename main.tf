6ffb0ddbd0c078abf837509bc6d4181667aea368
cloudlovely <lovely.kumari@clouddrove.com>
2023-09-08 14:51:32 +0530

    feat: add endpoint resource

--- main.tf
+++ main.tf
@@ -610,3 +610,35 @@ resource "aws_s3_bucket_analytics_configuration" "default" {
     }
   }
 }
+
+##----------------------------------------------------------------------------------
+## VPC Endpoint resource for S3.
+##----------------------------------------------------------------------------------
+data "aws_vpc_endpoint_service" "s3" {
+  for_each = var.vpc_endpoint
+  service      = "s3"
+  service_type = "Gateway"
+}
+
+resource "aws_vpc_endpoint" "this" {
+  for_each = var.endpoints
+
+  vpc_id            = var.vpc_id
+  service_name      = data.aws_vpc_endpoint_service.this[each.key].service_name
+  vpc_endpoint_type = try(each.value.service_type, "Interface")
+  auto_accept       = try(each.value.auto_accept, null)
+
+  security_group_ids  = try(each.value.service_type, "Interface") == "Interface" ? length(distinct(concat(local.security_group_ids, lookup(each.value, "security_group_ids", [])))) > 0 ? distinct(concat(local.security_group_ids, lookup(each.value, "security_group_ids", []))) : null : null
+  subnet_ids          = try(each.value.service_type, "Interface") == "Interface" ? distinct(concat(var.subnet_ids, lookup(each.value, "subnet_ids", []))) : null
+  route_table_ids     = try(each.value.service_type, "Interface") == "Gateway" ? lookup(each.value, "route_table_ids", null) : null
+  policy              = try(each.value.policy, null)
+  private_dns_enabled = try(each.value.service_type, "Interface") == "Interface" ? try(each.value.private_dns_enabled, null) : null
+
+  tags = merge(var.tags, try(each.value.tags, {}))
+
+  timeouts {
+    create = try(var.timeouts.create, "10m")
+    update = try(var.timeouts.update, "10m")
+    delete = try(var.timeouts.delete, "10m")
+  }
+}
