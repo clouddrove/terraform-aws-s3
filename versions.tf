1ac5569ee8677dfa41f62e3de377eacc22e0da32
clouddrove-ci <anmol+ci@clouddrove.com>
2026-05-15 12:59:15 +0000

    feat: modernize terraform-aws-s3 with provider v5.80 features [Confidence: 60/100]

--- versions.tf
+++ versions.tf
@@ -1,15 +1,5 @@
-# Terraform version
 terraform {
-  required_version = ">= 1.10.0"
-
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.80.0"
-    }
-  }
-
-  provider_meta "aws" {
-    user_agent = ["github.com/clouddrove/terraform-aws-s3"]
+    aws = { source = "hashicorp/aws" version = ">= 5.80.0" }
   }
 }
