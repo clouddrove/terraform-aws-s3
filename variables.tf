33a47e49b1bef02af68429d17d890971f1a11cb3
Anmol Nagpal <anmol@clouddrove.com>
2026-06-26 11:31:07 -0400

    fix(s3): secure defaults (#87)
    * feat: enable server-side encryption by default and improve docs
    
    * revert: README changes to keep original docs

--- variables.tf
+++ variables.tf
@@ -58,8 +58,8 @@ variable "acl" {
 
 variable "enable_server_side_encryption" {
   type        = bool
-  default     = false
-  description = "Enable enable_server_side_encryption"
+  default     = true
+  description = "Enable server-side encryption by default."
 }
 
 variable "sse_algorithm" {
@@ -71,7 +71,7 @@ variable "sse_algorithm" {
 variable "enable_kms" {
   type        = bool
   default     = false
-  description = "Enable enable_server_side_encryption"
+  description = "Enable KMS encryption. If false, uses AES256."
 }
 
 variable "kms_master_key_id" {
